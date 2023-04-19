8ec894be1a71c81368ef65dd0bf0ceca34aaf0ee
Eliot Valdes <eliottvaldes@hotmail.com>
2023-04-19 06:36:25 -0600

    feat: create index to export all models in a single file

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require('./user.model');
+// const Server = require('./server.model');
+const Role = require('./role.model');
+const Plan = require('./plan.model');
+
+module.exports = {
+    User,
+    // Server,
+    Role,
+    Plan,
+}
